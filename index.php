ed93a78d13b9723d349de8b104a665b397aa73f4
Antti Hätinen <pharazon@phz.fi>
2016-05-28 19:47:48 +0300

    Added SEPA XML parser

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * SEPA XML Parser for bank transfers (palkka.fi) pain.001.001.02 material
+ * Usage: Run from command line
+ * php index.php 3_28052016.xml
+ *
+ * Licence MIT
+ * @author Antti Hätinen <pharazon@phz.fi
+ */
+
+$xml = file_get_contents($argv[1]);
+$data = new SimpleXMLElement($xml);
+$payments = $data->{'pain.001.001.02'}->PmtInf->CdtTrfTxInf;
+
+$totalSum = 0.0;
+$count = 0;
+foreach ($payments as $payment) {
+    $sum = ((string) $payment->Amt->InstdAmt);
+    echo $sum . "\t" . $payment->Cdtr->Nm . PHP_EOL;
+    $totalSum += $sum;
+    $count++;
+}
+
+echo "Number of payments: " . $count . PHP_EOL;
+echo "Total sum: " . $totalSum . PHP_EOL;
